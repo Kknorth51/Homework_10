f78b38f64ae1186dda11797e051a2f17ab6759c2
kknorth <kirillacid@gmail.com>
2023-01-14 17:31:03 +0700

    Написал программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.

---
+++ ZADACHA_ITOG/Program.cs
@@ -0,0 +1,31 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых меньше либо равна 3 символа.
+
+string[] CreateArray()
+{
+    string[] array;
+    Console.WriteLine("Введите массив строк через пробел:");
+    string N = Console.ReadLine();
+    array = N.Split(' ');
+    return array;
+}
+void NewArrayStrings()
+{
+    string[] Array1 = CreateArray();
+    string str = string.Join(", ", Array1);
+    Console.Write($"[{str}] ->");
+    int count = 0;
+    string[] Array2 = new string[Array1.Length];
+    foreach (string i in Array1)
+    {
+        if (i.Length <= 3)
+        {
+            Array2[count] = i;
+            count++;
+        }
+    }
+    Array2 = Array2.Where(e => e != null).ToArray();
+    str = string.Join(", ", Array2);
+    Console.WriteLine($"[{str}]");
+}
+NewArrayStrings();
